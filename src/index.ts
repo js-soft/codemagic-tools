854777aeff74e05dd3e5d1f1fb535242d22f3d2f
Jakob Erben <jakob.erben@js-soft.com>
2023-11-17 11:31:06 +0100

    adding teams messaging feature via webhook

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 #!/usr/bin/env node
 
 import yargs from "yargs"
+import { TeamsMessaging } from "./teams/teams-messaging";
 
-const argv = yargs(process.argv.slice(2))
-  .options({ a: { type: "boolean", default: false } })
-  .parseSync()
 
-console.log(argv)
+const args = require("yargs").usage("jscm command")
+    .command( 'teams','This command is used to send a teams message via a passed webhook',
+    (args: any) => {
+        var caller = new TeamsMessaging();
+        args = caller.addTeamsCommandArguments(args);
+        caller.main(args.argv.webhook, args.argv.platform, args.argv.artifact_url, args.argv.was_build_successful);
+        return args;
+    })
+     // <insert options regarding all commands here>  .option('general-option', {description: 'a general option'})
+    .demand(1, "Must provide a valid command from the ones listed above.")
+    .version("v")
+    .alias("v","version")
+    .help("h")
+    .alias("h", "help")
+    .scriptName('jscm')
+    .argv;
+
+
+
+
