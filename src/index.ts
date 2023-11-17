6230f8099028c2c4b0f85760b3e867501159f0ac
Jakob Erben <jakob.erben@js-soft.com>
2023-11-17 13:17:12 +0100

    refactor: reformatting index file

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,20 @@
 #!/usr/bin/env node
 
 import yargs from "yargs"
-import { TeamsMessaging } from "./teams/teams-messaging";
+import { TeamsMessaging } from "./teams/TeamsMessaging"
 
-
-const args = require("yargs").usage("jscm command")
-    .command( 'teams','This command is used to send a teams message via a passed webhook',
-    (args: any) => {
-        var caller = new TeamsMessaging();
-        args = caller.addTeamsCommandArguments(args);
-        caller.main(args.argv.webhook, args.argv.platform, args.argv.artifact_url, args.argv.was_build_successful);
-        return args;
-    })
-     // <insert options regarding all commands here>  .option('general-option', {description: 'a general option'})
-    .demand(1, "Must provide a valid command from the ones listed above.")
-    .version("v")
-    .alias("v","version")
-    .help("h")
-    .alias("h", "help")
-    .scriptName('jscm')
-    .argv;
-
-
-
-
+const args = require("yargs")
+  .usage("jscm command")
+  .command("teams", "This command is used to send a teams message via a passed webhook", (args: any) => {
+    var caller = new TeamsMessaging()
+    args = caller.addTeamsCommandArguments(args)
+    caller.main(args.argv.webhook, args.argv.platform, args.argv.artifact_url, args.argv.was_build_successful)
+    return args
+  })
+  // <insert options regarding all commands here>  .option('general-option', {description: 'a general option'})
+  .demand(1, "Must provide a valid command from the ones listed above.")
+  .version("v")
+  .alias("v", "version")
+  .help("h")
+  .alias("h", "help")
+  .scriptName("jscm").argv
