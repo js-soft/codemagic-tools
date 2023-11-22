74e7b4f5c9348b62a21d6811d48dd2d851e4e4c4
Jakob Erben <jakob.erben@js-soft.com>
2023-11-22 09:41:18 +0100

    refactor: restructure for better readability

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,27 @@
 import yargs from "yargs"
 import { TeamsMessaging } from "./commands/TeamsMessaging"
 
-const functionToCall = function (args: any) {
-  const teamsMessagingCommand = new TeamsMessaging()
-  args = teamsMessagingCommand.defineCommandLineOptions(args)
-  const argv = args.argv
-  teamsMessagingCommand.main(argv.webhook, argv.platform, argv.artifactUrl, argv.wasBuildSuccessful).then(
-    () => {
-      // on fulfilled
-      console.log("Successful asynchronous execution")
-    },
-    (x) => {
-      // on rejection
-      console.log(x)
-      throw x
-    }
-  )
-  return args
+async function run() {
+  await yargs(process.argv.slice(2))
+    .command("teams", "This command is used to send a teams message via a passed webhook", async (args: any) => {
+      const teamsMessagingCommand = new TeamsMessaging()
+      args = teamsMessagingCommand.defineCommandLineOptions(args)
+      await teamsMessagingCommand.main(
+        args.argv.webhook,
+        args.argv.platform,
+        args.argv.artifactUrl,
+        args.argv.wasBuildSuccessful
+      )
+      return args
+    })
+    .demand(1, "Must provide a valid command from the ones listed above.")
+    .scriptName("jscm")
+    .parseAsync()
 }
 
-// eslint-disable-next-line no-void
-void yargs
-  .usage("jscm command")
-  .command("teams", "This command is used to send a teams message via a passed webhook", functionToCall)
-  .demand(1, "Must provide a valid command from the ones listed above.")
-  .scriptName("jscm").argv
+run()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
