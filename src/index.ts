74682c583e21909b8e6298ae161c6d9e2aff02fd
Jakob Erben <34318568+erbenjak@users.noreply.github.com>
2023-11-22 13:26:20 +0100

    Feature/Teams command (#2)
    * adding teams messaging feature via webhook
    
    * fix: removing wrong deactivation of provenance
    
    * refactor: correcting formating of the files
    
    * refactor: correcting file name
    
    * chore: removing unwanted file
    
    * refactor: reformatting index file
    
    * chore: increase version
    
    * refactor: correct folder name
    
    * refactor: fix folder name in import
    
    * refactor: proper reformat
    
    * refactor: improve promise handling
    
    * refactor: improve output-message
    
    * refactor: restructure for better readability
    
    * feat: define choices for platform argument
    
    * refactor: move platform choices to definition
    
    * refactor: cleanup
    
    ---------
    
    Co-authored-by: Julian König <33655937+jkoenig134@users.noreply.github.com>
    Co-authored-by: Julian König <julian.koenig@js-soft.com>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 #!/usr/bin/env node
 
 import yargs from "yargs"
+import { TeamsMessaging } from "./commands/TeamsMessaging"
 
-const argv = yargs(process.argv.slice(2))
-  .options({ a: { type: "boolean", default: false } })
-  .parseSync()
+async function run() {
+  await yargs(process.argv.slice(2))
+    .command("teams", "This command is used to send a teams message via a passed webhook", async (args) => {
+      const teamsMessagingCommand = new TeamsMessaging()
+      const options = await teamsMessagingCommand.parseCLIOptions(args)
+      await teamsMessagingCommand.run(options)
+      return options
+    })
+    .demand(1, "Must provide a valid command from the ones listed above.")
+    .scriptName("jscm")
+    .parseAsync()
+}
 
-console.log(argv)
+run()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
