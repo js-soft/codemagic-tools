28f483baa3e08930be7fe0a8538fbcf058972422
Julian König <julian.koenig@js-soft.com>
2023-11-22 12:34:03 +0100

    refactor: cleanup

--- src/commands/TeamsMessaging.ts
+++ src/commands/TeamsMessaging.ts
@@ -1,72 +1,81 @@
 import axios from "axios"
+import yargs from "yargs"
+
+export interface TeamsMessagingOptions {
+  webhook: string
+  platform: string
+  artifactUrl: string
+  wasBuildSuccessful: boolean
+}
 
 export class TeamsMessaging {
-  public async main(
-    webhook: string,
-    platformIdentifier: string,
-    artifactLink: string,
-    isSuccessful: boolean
-  ): Promise<boolean> {
-    if (!this.isSanityCheckUrlsOk([webhook, artifactLink])) {
-      console.log("ERROR: A given Url is not valid.")
-      return false
+  public async run(options: TeamsMessagingOptions): Promise<void> {
+    if (!this.isUrlValid(options.webhook)) {
+      console.error("The given webhook is not valid.")
+      process.exit(1)
     }
 
-    const statusIdentifier = isSuccessful ? "Successful" : "Failed"
+    if (!this.isUrlValid(options.artifactUrl)) {
+      console.error("The given artifactUrl is not valid.")
+      process.exit(1)
+    }
+
+    const statusIdentifier = options.wasBuildSuccessful ? "Successful" : "Failed"
+    const platformIdentifier = options.platform.toUpperCase()
 
     const messageContents = {
-      title: `New ${statusIdentifier.toUpperCase()} codemagic build - ${platformIdentifier.toUpperCase()}`,
-      summary: `${statusIdentifier.toUpperCase()} build - ${platformIdentifier.toUpperCase()}`,
-      text: isSuccessful
+      title: `New ${statusIdentifier.toLocaleLowerCase()} codemagic build - ${platformIdentifier}`,
+      summary: `${statusIdentifier} build - ${platformIdentifier}`,
+      text: options.wasBuildSuccessful
         ? "The newly released version did build and is now available as an artifact."
         : "A problem occurred while building the newly released version. The corresponding logs are available.",
       potentialAction: [
         {
           "@type": "OpenUri",
-          name: isSuccessful ? "Download Build" : "Download Logs",
-          targets: [{ os: "default", uri: artifactLink }]
+          name: options.wasBuildSuccessful ? "Download Build" : "Read Logs",
+          targets: [{ os: "default", uri: options.artifactUrl }]
         }
       ]
     }
 
-    await axios.post(webhook, messageContents)
-
-    return true
+    await axios.post(options.webhook, messageContents).catch((_) => {
+      console.log("Could not send message to teams channel.")
+      process.exit(1)
+    })
   }
 
-  public defineCommandLineOptions(args: any): any {
-    return args.option({
-      webhook: {
-        description: "the webhook of the teams channel, that should receive the message",
-        required: true,
-        type: "string"
-      },
-      platform: {
+  public parseCLIOptions(argv: yargs.Argv<{}>): TeamsMessagingOptions | Promise<TeamsMessagingOptions> {
+    return argv
+      .option("platform", {
         description: "identifier of the platform for which the build was created",
         required: true,
         type: "string",
         choices: ["ios", "android"]
-      },
-      artifactUrl: {
+      })
+      .option("artifactUrl", {
         description: "download link for the generated artifact (logs or build)",
         required: true,
         type: "string"
-      },
-      wasBuildSuccessful: { description: "status of the finished build", required: true, type: "boolean" }
-    })
+      })
+      .option("wasBuildSuccessful", {
+        description: "status of the finished build",
+        required: true,
+        type: "boolean"
+      })
+      .option("webhook", {
+        description: "the webhook of the teams channel, that should receive the message",
+        required: true,
+        type: "string"
+      }).argv
   }
 
-  private isSanityCheckUrlsOk(urls: string[]): boolean {
-    for (const urlToCheck of urls) {
-      try {
-        // eslint-disable-next-line no-new
-        new URL(urlToCheck)
-      } catch (err) {
-        console.log(err)
-        console.log(`Url causing the issue: ${urlToCheck}`)
-        return false
-      }
+  private isUrlValid(url: string): boolean {
+    try {
+      // eslint-disable-next-line no-new
+      new URL(url)
+      return true
+    } catch (err) {
+      return false
     }
-    return true
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,11 @@ import { TeamsMessaging } from "./commands/TeamsMessaging"
 
 async function run() {
   await yargs(process.argv.slice(2))
-    .command("teams", "This command is used to send a teams message via a passed webhook", async (args: any) => {
+    .command("teams", "This command is used to send a teams message via a passed webhook", async (args) => {
       const teamsMessagingCommand = new TeamsMessaging()
-      args = teamsMessagingCommand.defineCommandLineOptions(args)
-      await teamsMessagingCommand.main(
-        args.argv.webhook,
-        args.argv.platform,
-        args.argv.artifactUrl,
-        args.argv.wasBuildSuccessful
-      )
-      return args
+      const options = await teamsMessagingCommand.parseCLIOptions(args)
+      await teamsMessagingCommand.run(options)
+      return options
     })
     .demand(1, "Must provide a valid command from the ones listed above.")
     .scriptName("jscm")
