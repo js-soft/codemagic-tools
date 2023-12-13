23381970df9fd115057745c6871d11c1f1ce5e44
Jakob Erben <jakob.erben@js-soft.com>
2023-12-13 14:17:37 +0100

    refactor: reformat project

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,26 @@ import { TeamsMessagingPublish } from "./commands/TeamsMessagingPublish"
 
 async function run() {
   await yargs(process.argv.slice(2))
-    .command("teams-build", "After Codemagic Build: Send MS-Teams message informing about the new build", async (args) => {
-      const teamsMessagingCommand = new TeamsMessaging()
-      const options = await teamsMessagingCommand.parseCLIOptions(args)
-      await teamsMessagingCommand.run(options)
-      return options
-    })
-    .command("teams-publish", "After Codemagic Publish: Send MS-Teams message informing about the new release", async (args) => {
-      const teamsMessagePublish = new TeamsMessagingPublish()
-      const options = await teamsMessagePublish.parseCLIOptions(args)
-      await teamsMessagePublish.run(options)
-      return options
-    })
+    .command(
+      "teams-build",
+      "After Codemagic Build: Send MS-Teams message informing about the new build",
+      async (args) => {
+        const teamsMessagingCommand = new TeamsMessaging()
+        const options = await teamsMessagingCommand.parseCLIOptions(args)
+        await teamsMessagingCommand.run(options)
+        return options
+      }
+    )
+    .command(
+      "teams-publish",
+      "After Codemagic Publish: Send MS-Teams message informing about the new release",
+      async (args) => {
+        const teamsMessagePublish = new TeamsMessagingPublish()
+        const options = await teamsMessagePublish.parseCLIOptions(args)
+        await teamsMessagePublish.run(options)
+        return options
+      }
+    )
     .demand(1, "Must provide a valid command from the ones listed above.")
     .scriptName("jscm")
     .parseAsync()
